bfb7594a22765617a3b53afe84196be5455af85e
Mykola Grymalyuk <khronokernel@icloud.com>
2022-05-19 20:08:17 -0600

    Add Reference Binaries
    Not to be used by general public

---
+++ clean.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import subprocess
+
+folders_to_delete = [
+    # Mojave and Catalina non-Metal Patches
+    # No longer used by us, but nice for reference
+    "10.13.6-18",
+    "10.13.6-19",
+    "10.14.4-18",
+    "10.14.4-19",
+    "10.14.6-19",
+
+    # Nvidia Web Driver Patches
+    # Currently under development, no use for public distribution
+    "WebDriver-387.10.10.10.40.140",
+]
+
+def delete_folders():
+
+    root_path = "./Universal-Binaries"
+    for folder in folders_to_delete:
+        path = root_path + "/" + folder
+        if Path(path).exists():
+            print("Removing: " + path)
+            subprocess.run(["rm", "-rf", path])
+
+
+if __name__ == "__main__":
+    print("- Starting clean up")
+    delete_folders()
