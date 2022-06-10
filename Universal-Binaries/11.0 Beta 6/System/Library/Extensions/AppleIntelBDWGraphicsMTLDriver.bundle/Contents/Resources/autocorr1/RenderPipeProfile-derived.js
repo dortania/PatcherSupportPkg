dcb671a2421ce32577b71f72e12d9118212d4fd7
Mykola Grymalyuk <khronokernel@icloud.com>
2022-06-10 15:58:22 -0600

    Add Haswell, broadwell and Skylake binaries

---
+++ Universal-Binaries/11.0 Beta 6/System/Library/Extensions/AppleIntelBDWGraphicsMTLDriver.bundle/Contents/Resources/autocorr1/RenderPipeProfile-derived.js
@@ -0,0 +1,222 @@
+// Formulae to compute derived counters from raw counter values
+
+function GpuTime()
+{
+    return GpuTimeMD;
+}
+
+function GpuCoreClocks()
+{
+    return GpuCoreClocksMD;
+}
+
+function AvgGpuCoreFrequencyMHz()
+{
+    return AvgGpuCoreFrequencyMHzMD;
+}
+
+function GpuBusy()
+{
+    return GpuBusyMD;
+}
+
+function VsThreads()
+{
+    return VsThreadsMD;
+}
+
+function HsThreads()
+{
+    return HsThreadsMD;
+}
+
+function DsThreads()
+{
+    return DsThreadsMD;
+}
+
+function GsThreads()
+{
+    return GsThreadsMD;
+}
+
+function PsThreads()
+{
+    return PsThreadsMD;
+}
+
+function CsThreads()
+{
+    return CsThreadsMD;
+}
+
+function EuActive()
+{
+    return EuActiveMD;
+}
+
+function EuStall()
+{
+    return EuStallMD;
+}
+
+function RasterizedPixels()
+{
+    return RasterizedPixelsMD;
+}
+
+function HiDepthTestFails()
+{
+    return HiDepthTestFailsMD;
+}
+
+function EarlyDepthTestFails()
+{
+    return EarlyDepthTestFailsMD;
+}
+
+function SamplesKilledInPs()
+{
+    return SamplesKilledInPsMD;
+}
+
+function PixelsFailingPostPsTests()
+{
+    return PixelsFailingPostPsTestsMD;
+}
+
+function SamplesWritten()
+{
+    return SamplesWrittenMD;
+}
+
+function SamplesBlended()
+{
+    return SamplesBlendedMD;
+}
+
+function SamplerAccesses()
+{
+    return SamplerAccessesMD;
+}
+
+function SamplerTexels()
+{
+    return SamplerTexelsMD;
+}
+
+function SamplerTexelMisses()
+{
+    return SamplerTexelMissesMD;
+}
+
+function SlmBytesRead()
+{
+    return SlmBytesReadMD;
+}
+
+function SlmBytesWritten()
+{
+    return SlmBytesWrittenMD;
+}
+
+function ShaderMemoryAccesses()
+{
+    return ShaderMemoryAccessesMD;
+}
+
+function ShaderAtomics()
+{
+    return ShaderAtomicsMD;
+}
+
+function L3ShaderThroughput()
+{
+    return L3ShaderThroughputMD;
+}
+
+function ShaderBarriers()
+{
+    return ShaderBarriersMD;
+}
+
+function VfBottleneck()
+{
+    return VfBottleneckMD;
+}
+
+function VsBottleneck()
+{
+    return VsBottleneckMD;
+}
+
+function HsBottleneck()
+{
+    return HsBottleneckMD;
+}
+
+function DsBottleneck()
+{
+    return DsBottleneckMD;
+}
+
+function GsBottleneck()
+{
+    return GsBottleneckMD;
+}
+
+function SoBottleneck()
+{
+    return SoBottleneckMD;
+}
+
+function ClBottleneck()
+{
+    return ClBottleneckMD;
+}
+
+function SfBottleneck()
+{
+    return SfBottleneckMD;
+}
+
+function HiDepthBottleneck()
+{
+    return HiDepthBottleneckMD;
+}
+
+function EarlyDepthBottleneck()
+{
+    return EarlyDepthBottleneckMD;
+}
+
+function BcBottleneck()
+{
+    return BcBottleneckMD;
+}
+
+function HsStall()
+{
+    return HsStallMD;
+}
+
+function DsStall()
+{
+    return DsStallMD;
+}
+
+function SoStall()
+{
+    return SoStallMD;
+}
+
+function ClStall()
+{
+    return ClStallMD;
+}
+
+function SfStall()
+{
+    return SfStallMD;
+}
+
