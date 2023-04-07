5130a65affe42b00e5079ad0a0aea055d8dabb86
Mykola Grymalyuk <khronokernel@icloud.com>
2023-04-07 12:19:44 -0600

    Add 13.2.1’s GPUSupport libaries

---
+++ Universal-Binaries/13.2.1/System/Library/PrivateFrameworks/GPUCompiler.framework/Versions/31001/Libraries/lib/clang/31001.669/include/metal/metal_types.h
@@ -0,0 +1,15 @@
+// -*- Metal -*-
+//===-- metal_types_h -----------------------------------------------------===//
+// Copyright (c) 2014-2019 Apple Inc. All rights reserved
+//
+// This header is for internal use only. This header may disappear between
+// releases.
+//===----------------------------------------------------------------------===//
+
+
+#ifndef __METAL_TYPES_H
+#define __METAL_TYPES_H
+
+#include <metal_types>
+
+#endif // __METAL_TYPES_H
