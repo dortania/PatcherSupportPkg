b2df5fd6b02ea3efd60ca00370795a108fe83473
Mykola Grymalyuk <khronokernel@icloud.com>
2023-05-04 12:48:37 -0600

    CI: Adjust volume name

--- build-dmg.sh
+++ build-dmg.sh
@@ -2,6 +2,6 @@
 
 xattr -rc Universal-Binaries
 find Universal-Binaries -name .DS_Store -delete
-hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname binaries -fs HFS+ -ov -format UDRO
+hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname "OpenCore Patcher Resources (Root Patching)" -fs HFS+ -ov -format UDRO
 hdiutil convert -format ULMO tmp.dmg -o Universal-Binaries.dmg -passphrase password -encryption -ov
 rm tmp.dmg
