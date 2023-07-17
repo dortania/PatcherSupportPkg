8097a81f54d5d2fe6164d8e9f8c95ff07eeab26d
Mykola Grymalyuk <khronokernel@icloud.com>
2023-07-17 08:51:58 -0600

    Tooling: Increase DMG size to 4GB
    Allows for larger overlays during patching

--- build-dmg.sh
+++ build-dmg.sh
@@ -3,7 +3,7 @@
 # Homebrew...
 /usr/bin/xattr -rc Universal-Binaries
 find Universal-Binaries -name .DS_Store -delete
-hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname "OpenCore Patcher Resources (Root Patching)" -fs HFS+ -ov -format UDRO
+hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname "OpenCore Patcher Resources (Root Patching)" -fs HFS+ -ov -format UDRO -megabytes 4096
 hdiutil convert -format ULMO tmp.dmg -o Universal-Binaries.dmg -passphrase password -encryption -ov
 codesign -s "OpenCore Legacy Patcher Software Signing" Universal-Binaries.dmg
 rm tmp.dmg
