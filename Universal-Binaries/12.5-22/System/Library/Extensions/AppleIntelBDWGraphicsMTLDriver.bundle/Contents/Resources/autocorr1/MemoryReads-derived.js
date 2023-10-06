5c9fe86f3335fcfa027f4ae6021bbb7b3f3cec0b
Eduardo Covas <44532297+educovas@users.noreply.github.com>
2023-10-06 13:54:44 -0300

    Resolve rendering issues on Broadwell iGPUs

---
+++ Universal-Binaries/12.5-22/System/Library/Extensions/AppleIntelBDWGraphicsMTLDriver.bundle/Contents/Resources/autocorr1/MemoryReads-derived.js
@@ -0,0 +1,212 @@
+// Formulae to compute derived counters from raw counter values
+
+function GpuTime()
+{
+    return GpuTimeMD;
+}
+
+function GpuCoreClocks()
+{
+    return GpuCoreClocksMD;
+}
+
+function AvgGpuCoreFrequencyMHz()
+{
+    return AvgGpuCoreFrequencyMHzMD;
+}
+
+function GpuBusy()
+{
+    return GpuBusyMD;
+}
+
+function VsThreads()
+{
+    return VsThreadsMD;
+}
+
+function HsThreads()
+{
+    return HsThreadsMD;
+}
+
+function DsThreads()
+{
+    return DsThreadsMD;
+}
+
+function GsThreads()
+{
+    return GsThreadsMD;
+}
+
+function PsThreads()
+{
+    return PsThreadsMD;
+}
+
+function CsThreads()
+{
+    return CsThreadsMD;
+}
+
+function EuActive()
+{
+    return EuActiveMD;
+}
+
+function EuStall()
+{
+    return EuStallMD;
+}
+
+function RasterizedPixels()
+{
+    return RasterizedPixelsMD;
+}
+
+function HiDepthTestFails()
+{
+    return HiDepthTestFailsMD;
+}
+
+function EarlyDepthTestFails()
+{
+    return EarlyDepthTestFailsMD;
+}
+
+function SamplesKilledInPs()
+{
+    return SamplesKilledInPsMD;
+}
+
+function PixelsFailingPostPsTests()
+{
+    return PixelsFailingPostPsTestsMD;
+}
+
+function SamplesWritten()
+{
+    return SamplesWrittenMD;
+}
+
+function SamplesBlended()
+{
+    return SamplesBlendedMD;
+}
+
+function SamplerTexels()
+{
+    return SamplerTexelsMD;
+}
+
+function SamplerTexelMisses()
+{
+    return SamplerTexelMissesMD;
+}
+
+function SlmBytesRead()
+{
+    return SlmBytesReadMD;
+}
+
+function SlmBytesWritten()
+{
+    return SlmBytesWrittenMD;
+}
+
+function ShaderMemoryAccesses()
+{
+    return ShaderMemoryAccessesMD;
+}
+
+function ShaderAtomics()
+{
+    return ShaderAtomicsMD;
+}
+
+function ShaderBarriers()
+{
+    return ShaderBarriersMD;
+}
+
+function L3ShaderThroughput()
+{
+    return L3ShaderThroughputMD;
+}
+
+function GtiCmdStreamerMemoryReads()
+{
+    return GtiCmdStreamerMemoryReadsMD;
+}
+
+function GtiRsMemoryReads()
+{
+    return GtiRsMemoryReadsMD;
+}
+
+function GtiVfMemoryReads()
+{
+    return GtiVfMemoryReadsMD;
+}
+
+function GtiRccMemoryReads()
+{
+    return GtiRccMemoryReadsMD;
+}
+
+function GtiMscMemoryReads()
+{
+    return GtiMscMemoryReadsMD;
+}
+
+function GtiHizMemoryReads()
+{
+    return GtiHizMemoryReadsMD;
+}
+
+function GtiStcMemoryReads()
+{
+    return GtiStcMemoryReadsMD;
+}
+
+function GtiRczMemoryReads()
+{
+    return GtiRczMemoryReadsMD;
+}
+
+function GtiMemoryReads()
+{
+    return GtiMemoryReadsMD;
+}
+
+function GtiL3Bank0Reads()
+{
+    return GtiL3Bank0ReadsMD;
+}
+
+function GtiL3Bank1Reads()
+{
+    return GtiL3Bank1ReadsMD;
+}
+
+function GtiL3Bank2Reads()
+{
+    return GtiL3Bank2ReadsMD;
+}
+
+function GtiL3Bank3Reads()
+{
+    return GtiL3Bank3ReadsMD;
+}
+
+function GtiL3Reads()
+{
+    return GtiL3ReadsMD;
+}
+
+function GtiRingAccesses()
+{
+    return GtiRingAccessesMD;
+}
+
---
+++ Universal-Binaries/12.5-22/System/Library/Extensions/AppleIntelBDWGraphicsMTLDriver.bundle/Contents/Resources/autocorr1/Sampler_1-derived.js
@@ -0,0 +1,212 @@
+// Formulae to compute derived counters from raw counter values
+
+function GpuTime()
+{
+    return GpuTimeMD;
+}
+
+function GpuCoreClocks()
+{
+    return GpuCoreClocksMD;
+}
+
+function AvgGpuCoreFrequencyMHz()
+{
+    return AvgGpuCoreFrequencyMHzMD;
+}
+
+function GpuBusy()
+{
+    return GpuBusyMD;
+}
+
+function VsThreads()
+{
+    return VsThreadsMD;
+}
+
+function HsThreads()
+{
+    return HsThreadsMD;
+}
+
+function DsThreads()
+{
+    return DsThreadsMD;
+}
+
+function GsThreads()
+{
+    return GsThreadsMD;
+}
+
+function PsThreads()
+{
+    return PsThreadsMD;
+}
+
+function CsThreads()
+{
+    return CsThreadsMD;
+}
+
+function EuActive()
+{
+    return EuActiveMD;
+}
+
+function EuStall()
+{
+    return EuStallMD;
+}
+
+function EuFpuBothActive()
+{
+    return EuFpuBothActiveMD;
+}
+
+function VsFpu0Active()
+{
+    return VsFpu0ActiveMD;
+}
+
+function VsFpu1Active()
+{
+    return VsFpu1ActiveMD;
+}
+
+function VsSendActive()
+{
+    return VsSendActiveMD;
+}
+
+function PsFpu0Active()
+{
+    return PsFpu0ActiveMD;
+}
+
+function PsFpu1Active()
+{
+    return PsFpu1ActiveMD;
+}
+
+function PsSendActive()
+{
+    return PsSendActiveMD;
+}
+
+function PsEuBothFpuActive()
+{
+    return PsEuBothFpuActiveMD;
+}
+
+function RasterizedPixels()
+{
+    return RasterizedPixelsMD;
+}
+
+function HiDepthTestFails()
+{
+    return HiDepthTestFailsMD;
+}
+
+function EarlyDepthTestFails()
+{
+    return EarlyDepthTestFailsMD;
+}
+
+function SamplesKilledInPs()
+{
+    return SamplesKilledInPsMD;
+}
+
+function PixelsFailingPostPsTests()
+{
+    return PixelsFailingPostPsTestsMD;
+}
+
+function SamplesWritten()
+{
+    return SamplesWrittenMD;
+}
+
+function SamplesBlended()
+{
+    return SamplesBlendedMD;
+}
+
+function SamplerTexels()
+{
+    return SamplerTexelsMD;
+}
+
+function SamplerTexelMisses()
+{
+    return SamplerTexelMissesMD;
+}
+
+function SlmBytesRead()
+{
+    return SlmBytesReadMD;
+}
+
+function SlmBytesWritten()
+{
+    return SlmBytesWrittenMD;
+}
+
+function ShaderMemoryAccesses()
+{
+    return ShaderMemoryAccessesMD;
+}
+
+function ShaderAtomics()
+{
+    return ShaderAtomicsMD;
+}
+
+function L3ShaderThroughput()
+{
+    return L3ShaderThroughputMD;
+}
+
+function ShaderBarriers()
+{
+    return ShaderBarriersMD;
+}
+
+function Sampler11InputAvailable()
+{
+    return Sampler11InputAvailableMD;
+}
+
+function Sampler12InputAvailable()
+{
+    return Sampler12InputAvailableMD;
+}
+
+function Sampler10InputAvailable()
+{
+    return Sampler10InputAvailableMD;
+}
+
+function Sampler12OutputReady()
+{
+    return Sampler12OutputReadyMD;
+}
+
+function Sampler10OutputReady()
+{
+    return Sampler10OutputReadyMD;
+}
+
+function Sampler11OutputReady()
+{
+    return Sampler11OutputReadyMD;
+}
+
+function GTRequestQueueFull()
+{
+    return GTRequestQueueFullMD;
+}
+
