3906e86b53c604f40bc09c1f2befef18dc631bfe
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-10-15 17:52:42 -0400

    Bail on error

--- build-dmg.sh
+++ build-dmg.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
 # Homebrew...
-/usr/bin/xattr -rc Universal-Binaries
-find Universal-Binaries -name .DS_Store -delete
+/usr/bin/xattr -rc Universal-Binaries || true
+find Universal-Binaries -name .DS_Store -delete || true
 hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname "OpenCore Patcher Resources (Root Patching)" -fs HFS+ -ov -format UDRO -megabytes 4096
 hdiutil convert -format ULMO tmp.dmg -o Universal-Binaries.dmg -passphrase password -encryption -ov
 codesign -s "OpenCore Legacy Patcher Software Signing" Universal-Binaries.dmg
