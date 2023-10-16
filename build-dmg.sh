26e567c685f9d17c99fba434fb6f1549c34209da
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-10-15 23:13:04 -0400

    Remove any extended attributes that may exist

--- build-dmg.sh
+++ build-dmg.sh
@@ -6,5 +6,6 @@ set -e
 find Universal-Binaries -name .DS_Store -delete || true
 hdiutil create -srcfolder Universal-Binaries tmp.dmg -volname "OpenCore Patcher Resources (Root Patching)" -fs HFS+ -ov -format UDRO -megabytes 4096
 hdiutil convert -format ULMO tmp.dmg -o Universal-Binaries.dmg -passphrase password -encryption -ov
+/usr/bin/xattr -c Universal-Binaries.dmg || true
 codesign -s "OpenCore Legacy Patcher Software Signing" Universal-Binaries.dmg
 rm tmp.dmg
