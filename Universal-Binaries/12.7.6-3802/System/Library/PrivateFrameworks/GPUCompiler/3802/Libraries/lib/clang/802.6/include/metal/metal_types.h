f2d947304e09dc026094dcb12d90535f3db825f6
Mykola Grymalyuk <khronokernel@icloud.com>
2024-08-26 15:56:18 -0600

    Sync 3802 bins

---
+++ Universal-Binaries/12.7.6-3802/System/Library/PrivateFrameworks/GPUCompiler/3802/Libraries/lib/clang/802.6/include/metal/metal_types.h
@@ -0,0 +1,13 @@
+//===-- metal_types_h -----------------------------------------------------===//
+// Copyright (c) 2014-2017 Apple Inc. All rights reserved
+//===----------------------------------------------------------------------===//
+
+// TODO: This file is a temporary workaround. It is going to be removed soon --
+// see <rdar://problem/35032465>.
+
+#ifndef __METAL_TYPES_H
+#define __METAL_TYPES_H
+
+#include <metal_types>
+
+#endif // __METAL_TYPES_H
